f0de7a981ed750f5d1584bcccd4ac3a579a4e946
Alex Galin <satovritti@gmail.com>
2019-07-24 12:34:01 +0300

    removed redandand synchronized

--- src/main/scala/ru/arlen/cache/InMemoryCache.scala
+++ src/main/scala/ru/arlen/cache/InMemoryCache.scala
@@ -9,20 +9,20 @@ class InMemoryCache[K <: Serializable, V <: Serializable] extends Cache[K, V] {
   private val logger: Logger = getLogger
   private val cache          = new ConcurrentHashMap[K, V]().asScala
 
-  def clear(): Unit = this.synchronized {
+  def clear(): Unit = {
     logger.debug("clear in memory cache")
     cache.clear
   }
   def contains(key: K): Boolean = cache.contains(key)
-  def get(key: K): Option[V] = this.synchronized {
+  def get(key: K): Option[V] = {
     logger.debug(s"get key=${key}")
     cache.get(key)
   }
-  def put(key: K, value: V): Unit = this.synchronized {
+  def put(key: K, value: V): Unit = {
     logger.debug(s"put ${key} -> ${value}")
     cache.put(key, value)
   }
-  def remove(key: K): Unit = this.synchronized {
+  def remove(key: K): Unit = {
     logger.debug(s"remove ${key}")
     cache.remove(key)
   }
